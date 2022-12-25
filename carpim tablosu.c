c8af4d26350ae149278b77e3efa41d55e4a396ea
Esmanur <102920301+esmayric@users.noreply.github.com>
2022-12-25 16:43:44 +0300

    Add files via upload

---
+++ carpim tablosu.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	
+	int i,j;
+	
+	printf("=====CARPIM TABLOSU=====\n\n");
+	
+	for(i=1; i<=10;i++) {
+		
+		for(j=1;j<=10;j++) {
+			
+			printf("%d * %d = %d\n" ,i,j,i*j);
+		}
+		printf ("\n");
+	}
+	
+	return 0;
+}
